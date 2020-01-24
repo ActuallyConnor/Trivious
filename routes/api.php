432b3d6f0cfb4c0f4045b2c4a023c250fec8b08d
connor <connor@artscience.ca>
2020-01-24 12:18:04 -0500

    creating Question and Category models

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//Route::get('articles/{id}', function($id) {
+//    return Article::find($id);
+//});
